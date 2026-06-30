fbdfa80520d3556b8752dca970c79daabc38e471
Munirmahedi Paviwala <munir1612@gmail.com>
2026-06-30 18:46:56 +0530

    added more offline capability

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'daily-stitch-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.svg'
+];
+
+// Install event: cache all static assets
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        }).then(() => self.skipWaiting())
+    );
+});
+
+// Fetch event: serve from cache if available, otherwise fetch from network
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Activate event: clean up old caches and take control immediately
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
